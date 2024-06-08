8fdb7adfcfe6df543d064715fdbd6eaece0d5d81
Thanh262 <85877152+Thanh262@users.noreply.github.com>
2024-06-08 15:08:01 +0700

    Them model nnay oke ko

--- He_CSDL_DPT/Nhom_1_video/models.py
+++ He_CSDL_DPT/Nhom_1_video/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/')
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.title
